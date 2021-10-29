fe595d378fa9ec8ec84b3e778d26dd18e81c08b4
smallpondtom <usasmpdtom@gmail.com>
2021-10-29 01:56:56 -0400

    Edit function and format.:

--- python/quadrotorModel/utils.py
+++ python/quadrotorModel/utils.py
@@ -19,28 +19,13 @@
 
 # Modules *********************************************************************
 import numpy as np
-import numba as nb
 
-
-@nb.jit
-def inv_npmat_jit(mat: np.ndarray) -> np.ndarray:
-    """
-    inv_npmat_jit
-
-    Fast matrix inversion using numba and numpy.
-
-    Parameters
-    ----------
-    mat : np.ndarray
-        Input matrix.
-    """
-    return np.linalg.inv(mat)
 
 def skew_sym3(vec: np.ndarray) -> np.ndarray:
     """
-    skew_sym3     
+    skew_sym3
 
-    Transform a 3 dimensional vector to a skew symmetric matrix for cross 
+    Transform a 3 dimensional vector to a skew symmetric matrix for cross
     product manipulation.
 
     Parameters
@@ -53,15 +38,15 @@ def skew_sym3(vec: np.ndarray) -> np.ndarray:
     np.ndarray:
         Resulting skew symmetric matrix.
     """
-    a = np.reshape(vec, (1, 3))  # reshape the input matrix to 1 by 3
-    return np.ndarray([[0, a[3], a[2]], [a[3], 0, -a[1]], -a[2], a[1], 0])
+    a = np.reshape(vec, (3))  # reshape the input matrix to 1 by 3
+    return np.array([[0, -a[2], a[1]], [a[2], 0, -a[0]], [-a[1], a[0], 0]])
 
 
+def main():
+    a = np.array([[1], [3], [4]])
+    b = skew_sym3(a)
+    print(b)
 
 
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
